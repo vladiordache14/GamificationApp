c16e0dd571d1aa31e7100cc8ef0dbfdb72228b73
atletul <46723813+vladiordache14@users.noreply.github.com>
2023-04-04 12:29:03 +0300

    Initial commit

---
+++ src/main/java/app/gamificationapp/GamificationAppApplication.java
@@ -0,0 +1,13 @@
+package app.gamificationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamificationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamificationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/gamificationapp/GamificationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package app.gamificationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamificationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
